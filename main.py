6f368e7e991972a4787c1da350ca85d3325bb15e
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-05-31 21:32:39 +0300

    Set to use dark theme.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 from cbse import CBSE
+import sv_ttk
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.iconbitmap("./test.ico")
+    root.iconbitmap("test.ico")
     root.title("Copypasta Binder for Source Engine")
+    sv_ttk.use_dark_theme()
     app = CBSE(root)
     app.run()
