8d95e2835fb7c9262f49350445c710fd2081a5ad
Oscar <91684376+Oscar-97@users.noreply.github.com>
2024-01-24 21:55:27 +0200

    Fixed resource path.

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ from utils.cbse import CBSE
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
+    if hasattr(sys, '_MEIPASS'):
+        base_path = sys._MEIPASS  # pylint: disable=protected-access
+    else:
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
